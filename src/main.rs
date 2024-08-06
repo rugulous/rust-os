e63f11264cd5e14ea0e51c00190b85e3e07739ff
Luke Taylor <luke@circleleasing.co.uk>
2024-08-06 20:09:11 +0100

    Rebuild print / println macros

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 mod vga_buffer;
 
+use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern  "C" fn _start() -> ! {
-    vga_buffer::WRITER.lock().write_string("Hello again!");
-
+    println!("Hello World!");
+    panic!("Ruh roh");
     loop {}
 }
